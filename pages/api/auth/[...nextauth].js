8447514152253c91bc906282c80bb782eb521146
srhimel <himusr@gmail.com>
2022-08-09 21:44:08 +0600

    fixed auth secret

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,13 +2,14 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  secret: process.env.SECRET,
+
   pages: {
     signIn: "/auth/signin"
   }
