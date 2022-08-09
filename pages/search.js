d4a0ce27c6298e96aef15751a944da1b1eab63ac
srhimel <himusr@gmail.com>
2022-08-09 11:31:30 +0600

    added ssr google rest api to search page

--- pages/search.js
+++ pages/search.js
@@ -2,8 +2,10 @@ import Head from "next/head"
 import { useRouter } from "next/router"
 import React from "react"
 import SearchHeader from "../components/SearchHeader"
+import Response from "../Response"
 
-const Search = () => {
+const Search = ({ data }) => {
+  console.log(data)
   const router = useRouter()
   const { terms } = router.query
   return (
@@ -16,6 +18,24 @@ const Search = () => {
       <SearchHeader />
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const mockData = true
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.GOOGLE_API_KEY
+        }&cx=${process.env.GOOGLE_CSE_ID}&q=${context.query.terms}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((res) => res.json())
+  return {
+    props: {
+      data: data
+    }
+  }
 }
 
 export default Search
